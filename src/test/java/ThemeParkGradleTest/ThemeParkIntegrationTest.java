197456a81451bcec7d9a19d5cffbfe254553f669
TomStanley95 <tom.stanley@fdmgroup.com>
2020-05-06 17:34:12 +1000

    new tests

--- src/test/java/ThemeParkGradleTest/ThemeParkIntegrationTest.java
+++ src/test/java/ThemeParkGradleTest/ThemeParkIntegrationTest.java
@@ -1,18 +1,23 @@
-package ThemeParkGradle;
+package ThemeParkGradleTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import ThemePark.ThemeParkApplication;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=ThemeParkApplication.class)
 @AutoConfigureBefore
+@AutoConfigureMockMvc
 public class ThemeParkIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
